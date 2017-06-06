0e8c0b72874652e64ecdfbf10b55d3ad652966b6
github username <github email address>
2017-06-06 10:56:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+// given an array returns the minimum number
 function findMinAndRemove(array){
-
+  let min = array[0]
+  let minIndex = 0
+  for(let i = 0; i < array.length; i++){
+    let currentElement = array[i]
+    if(array[i] < min){
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
 }
 
+// given an array sorts each item and returns a sorted array
 function insertionSort(array){
-
+  let newMin;
+  let sorted = []
+  while(array.length != 0){
+    newMin = findMinAndRemove(array)
+    sorted.push(newMin)
+  }
+  return sorted;
 }
