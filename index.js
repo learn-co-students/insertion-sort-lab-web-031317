037dbd40e2d69ef24a253884f26539939c965bfe
christopher-hague <christopher.a.hague@gmail.com>
2017-06-06 09:54:55 -0400

    done

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function findMinAndRemove(array){
+  let min = array[0]
+  let minIndex = 0
 
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] < min) {
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return min
 }
 
 function insertionSort(array){
+  let min
+  let result = []
 
+  while (array.length != 0) {
+    min = findMinAndRemove(array)
+    result.push(min)
+  }
+
+  return result
 }
