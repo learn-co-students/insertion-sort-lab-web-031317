6ed90f520464d2e3328659fa5360310575456806
drumnation <davidmieloch@gmail.com>
2017-07-06 14:08:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function findMinAndRemove(array){
-
+const findMinAndRemove = (array) => {
+    let min = Math.min.apply(null, array)
+    let minIndex = array.indexOf(min)
+    array.splice(minIndex, 1)
+    return min
 }
 
-function insertionSort(array){
-
+const insertionSort = (array) => {
+    let min
+    let sorted = []
+    while (array.length !== 0) {
+        min = findMinAndRemove(array)
+        sorted.push(min)
+    }
+    return sorted
 }
